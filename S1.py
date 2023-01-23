2ed6f03af846fb24953a7e773503cb09034c9b23
Massoud Beygi <merkousha.net@gmail.com>
2023-01-23 18:12:16 +0330

    Create S1.py

---
+++ S1.py
@@ -0,0 +1,23 @@
+# Number Variable Types
+
+x= int(5)
+z=int(5.9)
+y=float(6)
+w= float(6.5)
+
+print(x)
+print(z)
+print(y)
+print(w)
+
+#Arrays
+a=[123,333,70,0,311,118,115]
+ChoosedIndex = int(input("Give me an index of Array: "))
+print(a[ChoosedIndex])
+
+#Dictionary
+Fruits ={"Apple":"Red","Orange":"Orange","Banana":"Yellow"}
+choosedFruit=input("Give me a Frouit Name:")
+print(Fruits[choosedFruit])
+
+
