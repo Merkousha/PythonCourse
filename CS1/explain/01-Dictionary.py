af2abedc64e0a9d0a5bd3bbf9f146a380580743a
Massoud Beygi <merkousha.net@gmail.com>
2023-02-21 18:33:37 +0330

    Create 01-Dictionary.py

---
+++ CS1/explain/01-Dictionary.py
@@ -0,0 +1,23 @@
+phonebook = {
+    "Asghar": "15511515",
+    "Akbar" : "12211212",
+    "hasan": "1551235",
+    "gholi" : "12214562",
+    "fati": "15511515",
+    "pari" : "1226573412",
+    "farangis": "2222511515",
+    "nosrat" : "9992211212",
+    "xxxxxxx" : 6,
+    "yyyyyyy" : 6.8
+    }
+
+person = {
+    "name":"massoud",
+    "Family":"beygi",
+    "Age" : 38,
+    "Score": 80.2,
+    "Gender": True
+}
+
+choosedname = input("give me a name :")
+print(phonebook[choosedname])
