1130f37b65ae78aa85bae9bb86c0eba3c324ec9a
Massoud Beygi <merkousha.net@gmail.com>
2023-02-26 19:26:31 +0330

    Create 03-for.py

---
+++ CS1/explain/03-for.py
@@ -0,0 +1,18 @@
+#For with simple range
+for i in range(1, 6):
+    print(i)
+    
+    
+#For with range  and step
+for i in range(0, 10, 2):
+    print(i)
+
+#For with List of Numbers
+numberlist =[1,3,6,3,10,10]
+for number in numberlist:
+    print(number)
+
+#For with List of Strings
+froutlist =["apple","banana","Orange","apple"]
+for frout in froutlist:
+    print(frout)
